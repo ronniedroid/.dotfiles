35a7b04b86c2a599625c2435e9d9ac1cf6fab25f
ronniedroid <ronni.n.yousif@gmail.com>
2020-04-04 19:57:16 +0300

    some tweaks

--- Scripts/Wallpaper.sh
+++ Scripts/Wallpaper.sh
@@ -5,33 +5,39 @@ DIR="$HOME/Pictures/"
 FILE=$(sxiv -t -o $DIR) 
 
 Center(){
-  xwallpaper --center $FILE
+  xwallpaper --center $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --center $FILE|" $HOME/.xinitrc
 }
 
 Stretch(){
-  xwallpaper --stretch $FILE
+  xwallpaper --stretch $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --stretch $FILE|" $HOME/.xinitrc
 }
 
 Tile(){
-  xwallpaper --tile $FILE
+  xwallpaper --tile $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --tile $FILE|" $HOME/.xinitrc
 }
 
 Zoom(){
-  xwallpaper --zoom $FILE
+  xwallpaper --zoom $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --zoom $FILE|" $HOME/.xinitrc
 }
 
-if [ -z $FILE ]
+No-randr(){
+  xwallpaper --no-randr --stretch $FILE  && sed -i "s|xwallpaper.*|xwallpaper
+  --no-randr --stretch $FILE|" $HOME/.xinitrc
+}
+
+if [ -z "$FILE" ]
 then
   exit 0
 else 
   $MENU
 fi
 
-MENU="dmenu -l 4"
-MODE=$(echo -e "center\nstretch\ntile\nzoom" | $MENU)
+MENU="dmenu -l 5"
+MODE=$(echo -e "center\nstretch\ntile\nzoom\nno-randr" | $MENU)
   case "$MODE" in
     center) Center ;;
     stretch) Stretch;;
     tile) Tile ;;
-    zoom) Zoom
+    zoom) Zoom ;;
+    no-randr) No-randr 
   esac
