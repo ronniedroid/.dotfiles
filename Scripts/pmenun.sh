84a93a3d997610cb908ba7c494986de4589dcb07
ronniedroid <ronni.n.yousif@gmail.com>
2020-04-20 09:34:48 +0300

    made a new powermenu

---
+++ Scripts/pmenun.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+Poweroff() {
+ MENU="dmenu -i -l 2 -p "-PowerOff?""
+ P=$(echo -e "YES\nNO\n" | $MENU)
+
+case "$P" in
+   YES) systemctl poweroff ;;
+    NO) exit 0;;
+  esac 
+}
+
+Reboot() {
+ MENU="dmenu -i -l 2 -p "-Reboot?""
+ R=$(echo -e "YES\nNO\n" | $MENU)
+
+case "$R" in
+   YES) systemctl poweroff ;;
+    NO) exit 0;;
+  esac 
+}
+
+Logout() {
+ MENU="dmenu -i -l 2 -p "-Logout?""
+ L=$(echo -e "YES\nNO\n" | $MENU)
+
+case "$L" in
+   YES) bspc quit ;;
+    NO) exit 0;;
+  esac 
+}
+
+MENU="dmenu -i -l 4 -p "-PowerMenu""
+PM=$(echo -e " Lock\n Logout\n Reboot\n Shutdown\n" | $MENU)
+    
+  case "$PM" in
+    *Poweroff) Poweroff ;;
+    *Reboot) Reboot ;;
+    *Logout) Logout 
+  esac 
