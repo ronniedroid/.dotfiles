c85d83f1c105f7358326a493b1d742e9c574b348
ronniedroid <ronni.n.yousif@gmail.com>
2020-04-26 14:55:59 +0300

    made some changes

--- Scripts/Wallpaper.sh
+++ Scripts/Wallpaper.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
 
-DIR="$HOME/Pictures/wallpapers/"
-FILE=$(sxiv -t -o $DIR) 
+DIR="$HOME/Pictures/wallpapers/" # Path to wallpapers
+FILE=$(sxiv -t -o $DIR | xargs)  # running sxiv, and marking either one or two files
+FILE1=$(printf "%s" "$FILE" | awk '{print $1}')  # Print the path to the first marked picture
+FILE2=$(printf "%s" "$FILE" | awk '{print $2}')  # Print the path to the second marked picture
+screen1=$(xrandr --listactivemonitors | grep "+" | awk '{print $4}' | awk NR==1) # Select the first screen for the dual function
+screen2=$(xrandr --listactivemonitors | grep "+" | awk '{print $4}' | awk NR==2) # Select the second screen if attached for the dual function
+
 
 Center(){
-  xwallpaper --center $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --center $FILE|" $HOME/.Scripts/defaultwallpaper.sh
+    xwallpaper --center $FILE1  && sed -i "s|xwallpaper.*|xwallpaper  --center $FILE1|" $HOME/.Scripts/defaultwallpaper.sh # set the first marked picture as wallpaper in Center mode and replace it in the defaultwallpaper script to make sure it loads with the x session
 }
 Stretch(){
-  xwallpaper --stretch $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --stretch $FILE|" $HOME/.Scripts/defaultwallpaper.sh
+    xwallpaper --stretch $FILE1  && sed -i "s|xwallpaper.*|xwallpaper  --stretch $FILE1|" $HOME/.Scripts/defaultwallpaper.sh # same as above but in Stretch mode
+
 }
 Tile(){
-  xwallpaper --tile $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --tile $FILE|" $HOME/.Scripts/defaultwallpaper.sh
+    xwallpaper --tile $FILE1  && sed -i "s|xwallpaper.*|xwallpaper  --tile $FILE1|" $HOME/.Scripts/defaultwallpaper.sh # same as above but in Tile mode
 }
 Zoom(){
-  xwallpaper --zoom $FILE  && sed -i "s|xwallpaper.*|xwallpaper  --zoom $FILE|" $HOME/.Scripts/defaultwallpaper.sh
+    xwallpaper --zoom $FILE1  && sed -i "s|xwallpaper.*|xwallpaper  --zoom $FILE1|" $HOME/.Scripts/defaultwallpaper.sh # same as above but in Zoom mode
+
 }
 No-randr(){
-  xwallpaper --no-randr --stretch $FILE  && sed -i "s|xwallpaper.*|xwallpaper --no-randr --stretch $FILE|" $HOME/.Scripts/defaultwallpaper.sh
+    xwallpaper --no-randr --stretch $FILE1  && sed -i "s|xwallpaper.*|xwallpaper --no-randr --stretch $FILE1|" $HOME/.Scripts/defaultwallpaper.sh # same as above but in No-randr mode
+
 }
 
+# issue here
+dual() {
+    xwallpaper --output $screen1 --zoom $FILE1 --output $screen2 --zoom $FILE2
+}
+
+# if no picture is marked in sxiv, exit.
 if [ -z "$FILE" ]
 then
   exit 0
-else 
+else
   $MENU
 fi
 
-MENU="dmenu -l 5"
-MODE=$(echo -e "center\nstretch\ntile\nzoom\nno-randr" | $MENU)
+MENU="dmenu -l 6"
+MODE=$(echo -e "center\nstretch\ntile\nzoom\nno-randr\ndual" | $MENU)
   case "$MODE" in
     center) Center ;;
     stretch) Stretch;;
     tile) Tile ;;
     zoom) Zoom ;;
-    no-randr) No-randr 
+    no-randr) No-randr ;;
+    dual) dual
   esac
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,9 @@ set PATH $HOME/.Scripts/ $PATH
 set PATH $HOME/.Scripts/statusbar $PATH
 set PATH $HOME/.emacs.d/bin/ $PATH
 
+export MANPAGER='nvim +Man!'
+export MANWIDTH=999
+
 PF_COL1=6 PF_COL2=7 PF_COL3=1 PF_SEP=":"  pfetch
 
 fish_vi_key_bindings
