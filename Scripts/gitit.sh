c063d1eb88e5e4bd53f778a727b72425eea5a3b9
ronniedroid <ronni.n.yousif@gmail.com>
2020-04-27 09:45:05 +0300

    added condition to check if git repo or not

--- Scripts/gitit.sh
+++ Scripts/gitit.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# Check and see if PWD is a git repository, and whether you are in the root of it or not.
+gittree=$(git rev-parse --is-inside-work-tree 2> /dev/null)
+
+if [ "$gittree" == true ]; then
+    if [ -d .git ]; then
+        $Choice
+    else
+        printf "%s" "Not in the root of your git repository"
+        exit 0
+    fi
+else
+    printf "%s" "Not a git repository."
+    exit 0
+fi
+
+# Print status in a clear way.
 gstatus() {
-    ST=$(git status | sed 's/modified://;s/new\ file://;s/[[:space:]]//g;/^[[:space:]]*$/d;/On.*/d;/(use.*/d;s/Changestobecommitted/Staged\ Changes/;s/Changesnotstagedforcommit/Unstaged\ Changes/;s/Untrackedfiles/Untracked Files/')
+    ST=$(git status | sed 's/modified://;s/new\ file://;s/deleted://;s/[[:space:]]//g;/^[[:space:]]*$/d;/On.*/d;/(use.*/d;s/Changestobecommitted/Staged\ Changes/;s/Changesnotstagedforcommit/Unstaged\ Changes/;s/Untrackedfiles/Untracked Files/;s/Yourbranchisuptodatewith/Up to do with remote repo\ /;s/Yourbranchisaheadof/Ahead of remote repo\ /;s/by/\ by\ /;s/commit/\ commit/')
     printf "%s\n" "$ST"
 }
 
