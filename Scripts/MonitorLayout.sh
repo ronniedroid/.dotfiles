39d65addac3857ea3b183284f9a4c1de4644b935
ronniedroid <ronni.n.yousif@gmail.com>
2020-04-29 09:38:31 +0300

    some small changes

--- Scripts/MonitorLayout.sh
+++ Scripts/MonitorLayout.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+screen1=$(xrandr --listactivemonitors | grep "+" | awk '{print $4}' | awk NR==1)
+screen2=$(xrandr --listactivemonitors | grep "+" | awk '{print $4}' | awk NR==2)
 
-MENU="dmenu -i -l 2 -p "ScreenLayout""
-C=$(echo -e "Dual\nSingle\n" | $MENU)
+MENU="dmenu -i -l 2 -p "Screen-Layout""
+MODE=$(echo -e "Dual\nSingle\n" | $MENU)
 
-	case "$C" in
-		*Dual) xrandr --output LVDS1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal && $HOME/.Scripts/defaultwallpaper.sh ;;
-		*Single) xrandr --output LVDS1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI1 --off
-	esac 
- 
+	case "$MODE" in
+		*Dual) xrandr --output $screen1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output $screen2 --mode 1920x1080 --pos 1366x0 --rotate normal && $HOME/.Scripts/defaultwallpaper.sh ;;
+		*Single) xrandr --output $screen1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output screen2 --off
+	esac
+
