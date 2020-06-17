3d12ce9bc237de44a1f67fd485d509a9e08c07b1
Ronnie Nissan <ronni.n.yousif@gmail.com>
2020-06-17 15:33:18 +0300

    added spectrwm config

--- Scripts/bmanager.sh
+++ Scripts/bmanager.sh
@@ -8,17 +8,46 @@ addnew() {
     echo $input >> $bookmarksf
 }
 
+duckgo() {
+menu="dmenu -i -l 10 -h 24 -p "DuckDuckGo""
+items=$($search | $menu )
 
-while getopts a: option; do
-    case "${option}" in
-        a) addnew && exit 0 ;;
-    esac
-done
+ [ -n "$items" ] && firefox duckduckgo/"$items" || exit 0
+}
 
-menu="dmenu -i -l 10 -p "Bookmarks""
+yt() {
+menu="dmenu -i -l 10 -h 24 -p "YouTube""
+items=$($search | $menu )
+
+ [ -n "$items" ] && firefox https://www.youtube.com/results?search_query="$items" || exit 0
+}
+
+bookmarks() {
+menu="dmenu -i -l 10 -h 24 -p "Bookmarks""
 items=$(cat $bookmarksf | awk -F "-" '{print $1}' | $menu )
 
 bms=$(cat $bookmarksf | grep "$items" | awk -F "-" '{print $2}')
 
 [ -n "$items" ] && firefox $bms || exit 0
+
+}
+
+rbookmark() {
+menu="dmenu -i -l 10 -p "Remove-a-Bookmarks""
+items=$(cat $bookmarksf | awk -F "-" '{print $1}' | $menu )
+
+[ -n "$items" ] && sed -i "/$items/d" "$bookmarksf" || exit 0
+echo $items $bookmarksf
+
+}
+
+while getopts a:bdyr option; do
+    case "${option}" in
+        a) addnew && exit 0 ;;
+        b) bookmarks ;;
+        d) duckgo ;;
+        y) yt ;;
+        r) rbookmark && exit 0 ;;
+    esac
+done
 
