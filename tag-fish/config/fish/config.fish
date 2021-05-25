b4adcc668db2d9cf3a4ce271ed8ecfa9e5c85b8f
ronniedroid <ronni.n.yousif@gmail.com>
2021-05-25 10:13:10 +0300

    some simple changes

---
+++ tag-fish/config/fish/config.fish
@@ -0,0 +1,15 @@
+function fish_greeting
+    pfetch
+end
+
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+
+fundle plugin 'PatrickF1/fzf.fish'
+fundle plugin 'jethrokuan/z'
+fundle plugin 'jorgebucaran/hydro'
+fundle plugin 'ryotako/fish-vimcolor'
+
+fundle init
+
+
