203d96e7025b4da2644ed8cb136ac209ca53c54a
ronniedroid <ronni.n.yousif@gmail.com>
2022-11-24 09:35:19 +0300

    Removed unneeded configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,15 @@
+
 function fish_greeting
 end
 
-set XDG_CURRENT_DESKTOP Unity
+fish_add_path ~/.local/bin
+fish_add_path ~/.local/bin/graalvm-ce-java17-22.1.0/bin
+fish_add_path ~/.cargo/bin
+fish_add_path ~/go/bin
 
+set -gx RCRC ~/.config
+set -gx JAVA_HOME ~/.local/bin/graalvm-ce-java17-22.1.0
+set XDG_CURRENT_DESKTOP Unity
 set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
 set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 
@@ -11,3 +18,9 @@ for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bi
         contains $flatpakdir $PATH; or set -a PATH $flatpakdir
     end
 end
+
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin 'jorgebucaran/hydro'
+
+fundle init
--- config/waybar/light.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brightness=$(light -G | awk -F "." '{print $1}')
-
-if grep -q open /proc/acpi/button/lid/LID/state; then
-    echo "" $brightness"%"
-else
-    echo "ﯥ"
-fi
